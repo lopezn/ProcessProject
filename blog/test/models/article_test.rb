7e7eb68e7697cdd9b00aeb6230675ee39de52365
Nico Lopez <lopezn@msoe.edu>
2015-01-21 15:48:52 -0600

    Model Tests

--- blog/test/models/article_test.rb
+++ blog/test/models/article_test.rb
@@ -1,7 +1,63 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	test "should have a title" do
+		expected = articles(:ValidArticle)
+		actual = Article.find(expected)
+
+		assert_equal expected.title, actual.title
+	end
+
+	test "should have text" do
+		expected = articles(:ValidArticle)
+		actual = Article.find(expected)
+
+		assert_equal expected.text, actual.text
+	end
+
+	test "should not save an article without a title" do
+		article = Article.new
+		assert_not article.save
+	end
+
+	test "should not save an article with a title whose length is less than 5" do
+		article = Article.new
+		article.title = "1234"
+		assert_not article.save
+	end
+
+	test "should save a valid article" do
+		article = Article.new
+		article.title = "12345"
+		assert article.save
+	end
+
+	test "can have no comments" do
+		actual = Article.find(articles(:WithNoComments))
+
+		assert_equal 0, actual.comments.count
+	end
+
+	test "can have one comment" do
+		actual = Article.find(articles(:WithOneComment))
+
+		assert_equal 1, actual.comments.count
+	end
+
+	test "can have multiple comments" do
+		actual = Article.find(articles(:WithTwoComments))
+
+		assert_equal 2, actual.comments.count
+	end
+
+	test "destroying an article destroys associated comments" do
+		actual = Article.find(articles(:WithOneComment))
+		comment_id = comments(:ForArticleWithOneComment).id
+
+		actual.destroy
+
+		assert_not Comment.exists?(comment_id)
+	end
+
 end
--- blog/test/models/comment_test.rb
+++ blog/test/models/comment_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have a commenter" do
+		expected = comments(:ValidComment)
+		actual = Comment.find(expected)
+
+		assert_equal expected.commenter, actual.commenter
+	end
+
+	test "should have a body" do
+		expected = comments(:ValidComment)
+		actual = Comment.find(expected)
+
+		assert_equal expected.body, actual.body
+	end
+
+	test "should belong to an article" do
+		comment = comments(:ValidComment)
+		expectedArticle = articles(:ValidArticle)
+
+		actualComment = Comment.find(comment)
+		actualArticle = Article.find(comment.article_id)
+
+		assert_equal expectedArticle, actualArticle
+	end
 end
